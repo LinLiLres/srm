31565282c1c90260891613cb24436bf1bae6d5dd
LinLires <lilin0618per@icloud.com>
2021-10-22 17:06:26 -0400

    first commend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '75d04319085cd8ba6568ce32e28528e7'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f7633b9a-8947-4fcf-8b14-5f71f8aa6e81'
+};
